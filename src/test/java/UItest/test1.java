deddaf83c54b8bd73892f5d58327e2d146029f31
Yusif <yusif.h@hotmail.com>
2023-03-30 15:22:10 -0400

    test 1 success

--- src/test/java/UItest/test1.java
+++ src/test/java/UItest/test1.java
@@ -1,7 +1,19 @@
 package UItest;
 
+import Utilities.ConfigurationReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class test1 {
     public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(ConfigurationReader.getProperty("env"));
+        driver.close();
 
     }
 }
